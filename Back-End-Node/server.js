1cd4ceda2b571a1fd6429966a1576564e31cca20
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-11-14 11:04:50 +0000

    Issue #5: database deployed and connected to the server

--- Back-End-Node/server.js
+++ Back-End-Node/server.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const port = process.env.PORT || 10000;
+const { pool } = require("./dbConfig");
 
-app.use(cors());
-const members = {
-    1: "Anna",
-    2: "Saim",
-    3:"Baki"
-};
+app.use(express.json());
+require("dotenv").config();
 
-// GET request to get all the members in the database
-
-app.get("/", (req, res) => {
-  res.send(members);
+//GET method below just to test if the server is running, when you create an endpoint you can delete this
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM public.city");
+    //I received the output '{"user":"postgres"}' without 'public' preceding 'user' because PostgreSQL defaults to a behavior where referencing a 'user' table without specifying a schema results in the default system view or object named 'user' being referenced inadvertently. To explicitly refer to the 'user' table, I utilized the table's schema name or qualified the table name with the schema where it exists.
+    res.send(result.rows);
+  } catch (error) {
+    res.status(500).send("Error inserting data");
+    console.error("Error executing query:", error);
+  }
 });
+
+
+const port = process.env.PORT || 10000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
