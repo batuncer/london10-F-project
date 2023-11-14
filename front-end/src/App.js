4a827d8bac775ed31cf7f7e46a94c3712c52d975
batuncer <tuncerbaki@gmail.com>
2023-11-14 21:50:30 +0000

    #9 Design REact component architecture. I have started and I will use material mui too.I added css files for every pages, If some of us doesnt want to use mui.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,20 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/header/header";
+import MainRoutes from "./routes";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://f-project-node.onrender.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Press here to go to the back-end first link
-        </a>
-      </header>
+      <MainRoutes />
     </div>
   );
 }
---
+++ front-end/src/pages/login/login.jsx
@@ -0,0 +1,14 @@
+// Login.jsx
+import React from 'react';
+import LoginForm from '../../components/auth-components/LoginForm';
+
+const Login = () => {
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default Login;
