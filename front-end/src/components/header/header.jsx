b5747d6dbfe0bab9555a785ca62239d855fa6dd0
AnnaFYZ <113105718+AnnaFYZ@users.noreply.github.com>
2023-11-20 20:52:21 +0000

    header added

--- front-end/src/components/header/header.jsx
+++ front-end/src/components/header/header.jsx
@@ -1,30 +1,41 @@
 import { AppBar, Box, Button, IconButton, Toolbar, Typography } from "@mui/material";
 import React from "react";
 import MenuIcon from '@mui/icons-material/Menu';
+import logo from "./cyf_brand.a79342f2f7bf2dee2c68.png";
 
-const Header = () => {
-    return (
-        
-<Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            CodeYourFuture
-          </Typography>
-          <Button color="inherit">Support</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-    )
+export default function Header() {
+  return (
+    <div className="App-header">
+        <img src={logo} className="logoImg" alt="CYF-logo" />
+        <h2>CYF Saturday Sessions Planner</h2>
+    </div>
+  );
 }
 
-export default Header
+
+// const Header = () => {
+//     return (
+        
+// <Box sx={{ flexGrow: 1 }}>
+//       <AppBar position="static">
+//         <Toolbar>
+//           <IconButton
+//             size="large"
+//             edge="start"
+//             color="inherit"
+//             aria-label="menu"
+//             sx={{ mr: 2 }}
+//           >
+//             <MenuIcon />
+//           </IconButton>
+//           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+//             CodeYourFuture
+//           </Typography>
+//           <Button color="inherit">Support</Button>
+//         </Toolbar>
+//       </AppBar>
+//     </Box>
+//     )
+// }
+
+// export default Header
