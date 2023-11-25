4080f64fe8fcb4e9cf0502dfb7f090c06bd18461
nsaimk <nsaimkorkmaz1@gmail.com>
2023-11-25 16:12:59 +0000

    myUrlWithParams is live

--- front-end/src/components/auth-components/SlackLoginButton.jsx
+++ front-end/src/components/auth-components/SlackLoginButton.jsx
@@ -8,16 +8,16 @@ export default function SlackLoginButton() {
     // const slackClientId = "6209798254180.6224787694115"
     console.log(slackClientId)
 
-    // const myUrlWithParams = new URL("https://slack.com/oauth/v2/authorize");
+    const myUrlWithParams = new URL("https://slack.com/oauth/v2/authorize");
 
-    // myUrlWithParams.searchParams.append("user_scope", "identity.basic,identity.email");
+    myUrlWithParams.searchParams.append("user_scope", "identity.basic,identity.email");
     myUrlWithParams.searchParams.append("redirect_url", `${process.env.BACK_END_URL}/auth/redirect`);
-    // myUrlWithParams.searchParams.append("client_id", slackClientId);
+    myUrlWithParams.searchParams.append("client_id", slackClientId);
 
-    // console.log(myUrlWithParams.href);
-    // let slack_url = myUrlWithParams.href;
+    console.log(myUrlWithParams.href);
+    let slack_url = myUrlWithParams.href;
 
-    let slack_url = `https://slack.com/oauth/v2/authorize?user_scope=identity.basic,identity.email&redirect_uri=https%3A%2F%2Flocalhost%3A443%2Fauth%2Fredirect&client_id=${slackClientId}`
+    //let slack_url = `https://slack.com/oauth/v2/authorize?user_scope=identity.basic,identity.email&redirect_uri=https%3A%2F%2Flocalhost%3A443%2Fauth%2Fredirect&client_id=${slackClientId}`
     //https://testworkspace-9th7274.slack.com/oauth?client_id=6209798254180.6224787694115&scope=&user_scope=users%3Aread%2Cusers.profile%3Aread&redirect_uri=https%3A%2F%2Flocalhost%3A443%2Fauth%2Fredirect&state=&granular_bot_scope=1&single_channel=0&install_redirect=&tracked=1&team=1
 
     function openPopup() {
